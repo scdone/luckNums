f4986b94e4dbf18e42a15736a6368e592ad40f0d
Stetson Done <scdone@gmail.com>
2021-12-07 07:24:48 -0700

    luckyNumber function solved

--- findLuckyNumbers.js
+++ findLuckyNumbers.js
@@ -1,1 +1,25 @@
-// Write your code below this line.
+
+// Instructions:
+
+// Given a number, n, return an array containing n unique random numbers between 1-10, inclusive. (That is, do not repeat any numbers in the returned list.)
+
+// You can trust that this function will never be called with n < 0 or n > 10.
+
+// For example:
+
+// luckyNumbers(2)
+// // returns (3, 7)
+// luckyNumbers(6)
+// returns (1, 7, 9, 6, 5, 2)
+
+function luckyNumbers (num) {
+let newArray = [];
+while (newArray.length < num) {
+    let randomNums = Math.floor((Math.random()*(10) + 1))
+    newArray.push(randomNums);
+}
+console.log(newArray);
+}
+
+luckyNumbers(5)
+
