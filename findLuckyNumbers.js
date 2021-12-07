317d701bbe2b83fc59ef2de88912af6d871af329
Stetson Done <scdone@gmail.com>
2021-12-07 07:56:32 -0700

    solution for unique numbers added

--- findLuckyNumbers.js
+++ findLuckyNumbers.js
@@ -23,3 +23,15 @@ console.log(newArray);
 
 luckyNumbers(5)
 
+// solution below for unique numbers, provided by instructor
+
+function findLuckyNumbers(n) {
+    let possibleNumbers = [1,2,3,4,5,6,7,8,9,10]
+    let luckyNumbers = []
+    for (let i = 0; i < n; i++) {
+      let x = Math.floor(Math.random() * possibleNumbers.length);
+      luckyNumbers.push(possibleNumbers[x])
+      possibleNumbers.splice(x, 1)
+    }
+    return luckyNumbers
+  }
